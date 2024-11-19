ec5e54d864921ba20e4d98717eb9573f801f1f74
Sbsahar <samo3696385@gmail.com>
2024-11-19 04:35:55 +0300

    Create config.py

---
+++ advbot/config.py
@@ -0,0 +1,17 @@
+
+TOKEN = "7707935653:AAG96LC9Z85IFBCdFWij9dVBv1X_-sTAxfc"
+CHANNEL_ID = "@QUEEN_SH963"  # معرف القناة بدون روابط
+ADMIN_ID = 6789179634  # ضع معرف المطور هنا
+
+
+message_limit_per_24h = 2
+# قائمة الإعدادات
+settings = {
+    "accept_stickers": True,
+    "accept_media": True,
+    "accept_text": True,
+    "banned_users": set()
+}
+# تخزين اللايكات
+likes_count = {}
+user_likes = {}  # قاموس لتتبع المستخدمين الذين أعجبوا بالرسائل
